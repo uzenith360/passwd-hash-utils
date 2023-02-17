2d525c28ce9b16798a0a24259eb9abdcabe58368
Zenith Wogwugwu <uzenith360@gmail.com>
2023-02-17 01:58:18 +0100

    fix: remove bcypt, use inbuild crypto lib instead

--- src/passwd-hash-utils.ts
+++ src/passwd-hash-utils.ts
@@ -1,42 +1,52 @@
-import * as bcrypt from "bcrypt";
+import { scrypt, randomBytes, timingSafeEqual } from "crypto";
 
 export default class PasswdHashUtils {
     private static _instance: PasswdHashUtils;
 
-    private readonly saltRounds: number;
+    // private readonly saltRounds: number;
 
-    private constructor(saltRounds: number) {
-        this.saltRounds = saltRounds;
+    private constructor(/*saltRounds: number*/) {
+        // this.saltRounds = saltRounds;
     }
 
-    public hash(password: string): Promise<string> {
+    public hash(
+        password: string,
+        salt: string = randomBytes(16).toString("hex")
+    ): Promise<string> {
         return new Promise(
             (resolve, reject) => {
-                bcrypt.genSalt(this.saltRounds, (err, salt) => {
-                    if (err) {
-                        return reject(err);
-                    }
-
-                    bcrypt.hash(password, salt, (err, hash) => {
+                scrypt(
+                    password,
+                    salt,
+                    64,
+                    (err: Error | null, derivedKey: Buffer) => {
                         if (err) {
                             return reject(err);
                         }
 
                         // Store hash in your password DB.
-                        return resolve(hash);
-                    });
-                });
+                        return resolve(`${derivedKey.toString("hex")}.${salt}`);
+                    },
+                );
             },
         );
     }
 
-    public compare(password: string, hash: string): Promise<boolean> {
-        return bcrypt.compare(password, hash);
+    public async compare(password: string, hash: string): Promise<boolean> {
+        const [hashedPassword, salt] = hash.split(".");
+        // we need to pass buffer values to timingSafeEqual
+        const hashedPasswordBuf = Buffer.from(hashedPassword, "hex");
+        // we hash the new sign-in password
+        const suppliedPasswordBuf: Buffer
+            = Buffer.from((await this.hash(password, salt)).split('.')[0], 'hex');
+
+        // compare the new supplied password with the stored hashed password
+        return timingSafeEqual(hashedPasswordBuf, suppliedPasswordBuf);
     }
 
-    public static getInstance(saltRounds: number): PasswdHashUtils {
+    public static getInstance(/*saltRounds: number*/): PasswdHashUtils {
         if (!PasswdHashUtils._instance) {
-            PasswdHashUtils._instance = new PasswdHashUtils(saltRounds);
+            PasswdHashUtils._instance = new PasswdHashUtils(/*saltRounds*/);
         }
 
         return PasswdHashUtils._instance;
