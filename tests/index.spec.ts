15bdbc4932c36e9e3e3557b4f4f56c7a4253acc1
Zenith Wogwugwu <uzenith360@gmail.com>
2023-03-04 03:11:18 +0100

    fix: compare bug

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -7,4 +7,13 @@ describe('Passwd hash Utils Class', () => {
     it('should have a getInstance init method', () => {
         assert.isFunction(PasswdHashUtils.getInstance);
     });
+
+    it('should correctly compare a password and hash', () => {
+        const passwordHashUtil: PasswdHashUtils = PasswdHashUtils.getInstance();
+        const password: string = '1234567890asdffg';
+        const [hash, salt] = passwordHashUtil.hash(password).split('.')
+
+        assert.isTrue(passwordHashUtil.compare(password, hash));
+        assert.isFalse(passwordHashUtil.compare('password', hash));
+    });
 });
